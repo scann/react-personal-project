e8b0871e7e61fb362ecd411930594e3da3c86c88
scann <apple.eater01@gmail.com>
2018-11-27 16:34:36 +0200

    Task component added and styled

--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -3,6 +3,10 @@ import React, { PureComponent } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
+import Checkbox from "../../theme/assets/Checkbox";
+import Star from '../../theme/assets/Star';
+import Edit from '../../theme/assets/Edit';
+import Remove from '../../theme/assets/Remove';
 
 export default class Task extends PureComponent {
     _getTaskShape = ({
@@ -18,6 +22,35 @@ export default class Task extends PureComponent {
     });
 
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        return (
+            <li className = { Styles.task }>
+                <div className = { Styles.content }>
+                    <div className = { Styles.toggleTaskCompletedState }>
+                        <Checkbox
+                            inlineBlock
+                            color1 = '#3b8ef3'
+                            color2 = '#FFF'
+                        />
+                    </div>
+                    <input maxLength = { 50 } type = 'text' />
+                </div>
+                <div className = { Styles.actions }>
+                    <Star
+                        inlineBlock
+                        color1 = '#3b8ef3'
+                        className = { Styles.toggleTaskFavoriteState }
+                    />
+                    <Edit
+                        inlineBlock
+                        color1 = '#3b8ef3'
+                        className = { Styles.updateTaskMessageOnClick }
+                    />
+                    <Remove
+                        inlineBlock
+                        color1 = '#3b8ef3'
+                    />
+                </div>
+            </li>
+        );
     }
 }
