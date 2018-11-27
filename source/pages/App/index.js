c87300c0e60faa80099c22f6c32f45c80a867636
scann <apple.eater01@gmail.com>
2018-11-27 16:37:23 +0200

    Scheduler component added to App page

--- source/pages/App/index.js
+++ source/pages/App/index.js
@@ -2,12 +2,15 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import Scheduler from "../../components/Scheduler";
+import Task from "../../components/Task";
 
 @hot(module)
 export default class App extends Component {
     render () {
         return (
-            <Scheduler />
+            <Scheduler>
+                <Task />
+            </Scheduler>
         );
     }
 }
