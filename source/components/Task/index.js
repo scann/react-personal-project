b38f84392df24d83d79c06038fea5693e0b86655
scann <apple.eater01@gmail.com>
2018-11-28 15:27:27 +0200

    Task component - props validation and state added

--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -1,5 +1,6 @@
 // Core
 import React, { PureComponent } from 'react';
+import { string, bool } from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
@@ -9,6 +10,18 @@ import Edit from '../../theme/assets/Edit';
 import Remove from '../../theme/assets/Remove';
 
 export default class Task extends PureComponent {
+    static propTypes = {
+        completed: bool.isRequired,
+        favorite:  bool.isRequired,
+        id:        string.isRequired,
+        message:   string.isRequired,
+    };
+
+    state = {
+        newMessage:    this.props.message,
+        isTaskEditing: false,
+    };
+
     _getTaskShape = ({
         id = this.props.id,
         completed = this.props.completed,
@@ -22,32 +35,47 @@ export default class Task extends PureComponent {
     });
 
     render () {
+        const { isTaskEditing, newMessage } = this.state;
+        const { message, favorite, completed } = this.props;
+
+        const currentMessage = isTaskEditing ? newMessage : message;
+
         return (
             <li className = { Styles.task }>
                 <div className = { Styles.content }>
-                    <div className = { Styles.toggleTaskCompletedState }>
-                        <Checkbox
-                            inlineBlock
-                            color1 = '#3b8ef3'
-                            color2 = '#FFF'
-                        />
-                    </div>
-                    <input maxLength = { 50 } type = 'text' />
+                    <Checkbox
+                        inlineBlock
+                        checked = { completed }
+                        className = { Styles.toggleTaskCompletedState }
+                        color1 = '#3b8ef3'
+                        color2 = '#FFF'
+                    />
+                    <input
+                        disabled = { !isTaskEditing }
+                        maxLength = { 50 }
+                        type = 'text'
+                        value = { currentMessage }
+                    />
                 </div>
                 <div className = { Styles.actions }>
                     <Star
                         inlineBlock
-                        color1 = '#3b8ef3'
+                        checked = { favorite }
                         className = { Styles.toggleTaskFavoriteState }
+                        color1 = '#3B8EF3'
+                        color2 = '#000'
                     />
                     <Edit
                         inlineBlock
-                        color1 = '#3b8ef3'
+                        checked = { isTaskEditing }
                         className = { Styles.updateTaskMessageOnClick }
+                        color1 = '#3B8EF3'
+                        color2 = '#000'
                     />
                     <Remove
                         inlineBlock
-                        color1 = '#3b8ef3'
+                        color1 = '#3B8EF3'
+                        color2 = '#000'
                     />
                 </div>
             </li>
