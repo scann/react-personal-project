62ec2b1d531aad5ea0cc80cd05de8d4a84b22ca1
scann <apple.eater01@gmail.com>
2018-12-12 16:11:05 +0200

    createTask function added

--- source/REST/api.js
+++ source/REST/api.js
@@ -1,2 +1,17 @@
+import { TOKEN, MAIN_URL } from "./config";
+
 export const api = {
+    async createTask (newTaskMessage) {
+        const response = await fetch(MAIN_URL, {
+            method:  'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+            },
+            body: JSON.stringify({ message: newTaskMessage }),
+        });
+        const { data: task } = await response.json();
+
+        return task;
+    },
 };
